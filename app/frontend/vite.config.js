0be1d5284d55fd7c3acea3c1578f5ca1eed0e8cf
Johannes Brosi <mail@jbrosi.ch>
2025-12-15 22:16:47 +0100

    add frontend

---
+++ app/frontend/vite.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite';
+import checker from 'vite-plugin-checker';
+import solid from 'vite-plugin-solid';
+
+export default defineConfig({
+  plugins: [
+    solid(),
+    checker({
+      typescript: true,
+    }),
+  ],
+  server: {
+    host: true,
+    port: 5173,
+    strictPort: true,
+  },
+});
