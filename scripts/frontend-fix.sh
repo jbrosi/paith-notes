d7d592eaf54a54a9f2fef28a124dd2477d245ab3
Johannes Brosi <mail@jbrosi.ch>
2025-12-17 21:51:02 +0100

    style

--- scripts/frontend-fix.sh
+++ scripts/frontend-fix.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 set -eu
 
-docker compose run --rm --no-deps frontend sh -lc "corepack enable && yarn install && yarn check"
+docker compose run --rm --no-deps frontend sh -lc "corepack enable && yarn install && yarn run check"
