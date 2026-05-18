90dd27c1cd1f9bd579acad869f5148f4755f322b
Johannes Brosi <mail@jbrosi.ch>
2026-05-18 21:27:43 +0200

    fefe

--- app/frontend/cypress.config.ts
+++ app/frontend/cypress.config.ts
@@ -13,5 +13,7 @@ export default defineConfig({
 		baseUrl: "http://localhost:8000",
 		specPattern: "cypress/e2e/**/*.cy.{ts,tsx}",
 		supportFile: "cypress/support/e2e.ts",
+		viewportWidth: 1280,
+		viewportHeight: 720,
 	},
 });
